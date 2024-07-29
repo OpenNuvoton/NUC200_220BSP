6928d860624cce2cff11ff697b5b5aed67843ec6
“YTLu” <YTLu@nuvoton.com>
2024-07-29 14:17:32 +0800

    Modify Readme, Revision history and chm files.
    Change-Id: Ic60fa24d40584e9488462233ae6f97807e925c0c

--- Library/Device/Nuvoton/NUC200Series/Include/NUC200Series.h
+++ Library/Device/Nuvoton/NUC200Series/Include/NUC200Series.h
@@ -7,7 +7,6 @@
  *
  * @note
  * @copyright SPDX-License-Identifier: Apache-2.0
- *
  * @copyright Copyright (C) 2014 Nuvoton Technology Corp. All rights reserved.
  ******************************************************************************/
 
@@ -29,7 +28,7 @@
   *
   * <b>Copyright Notice</b>
   *
-  * Copyright (C) 2014 Nuvoton Technology Corp. All rights reserved.
+  * Copyright (C) 2014~2024 Nuvoton Technology Corp. All rights reserved.
   */
 
 #ifndef __NUC200Series_H__
