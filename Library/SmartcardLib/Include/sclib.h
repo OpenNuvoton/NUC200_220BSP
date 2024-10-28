de88d20c93934b7e43aab5fdb82742bd61ed1639
“YTLu” <YTLu@nuvoton.com>
2024-10-28 10:30:02 +0800

    Update the sc library to fix can't run code.
    Change-Id: Ib832e9ff6a190812806e2209147de331a4bfeb22

--- Library/SmartcardLib/Include/sclib.h
+++ Library/SmartcardLib/Include/sclib.h
@@ -6,8 +6,7 @@
  * @brief    Smartcard library header file
  *
  * @note
- * @copyright SPDX-License-Identifier: Apache-2.0
- * @copyright Copyright (C) 2014 Nuvoton Technology Corp. All rights reserved.
+ * Copyright (C) 2013 Nuvoton Technology Corp. All rights reserved.
  *****************************************************************************/
 #ifndef __SCLIB_H__
 #define __SCLIB_H__
